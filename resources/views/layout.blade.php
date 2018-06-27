792e7130a98c3d35342261792ea84fe8eb7e60b8
akihizashi <38058168+akihizashi@users.noreply.github.com>
2018-06-27 14:13:18 +0900

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
   <title>Team Lab</title>
   <style>
       html, body {
